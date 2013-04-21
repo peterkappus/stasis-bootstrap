a0c02768e375d61a8c30028c3a60f479c3c5cd3a
Peter Kappus <peterk@peterkappus.com>
2013-04-21 21:36:39 +0100

    remove GA code and music specific stuff

--- controller.rb
+++ controller.rb
@@ -2,3 +2,10 @@ layout 'layout.html.slim'
 ignore /.git/
 ignore /\/_.*/
 ignore 'README.md'
+
+#here's how to make helpers that you can use in your views
+helpers do
+  def say_hello
+    'Hello'
+  end
+end
